15fec4c81310890fc9c01bdade58d91a709d9182
Olga Bobrova <eve982@yandex.ru>
2023-02-17 01:29:28 +0300

    changed cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,16 +1,10 @@
-const allowedCors = [
-  'https://eve982.pet-project.nomoredomains.work/',
-  'http://eve982.pet-project.nomoredomains.work/',
-  'http://localhost:3000',
-  'https://localhost:3000',
-];
-
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+const { allowedCors } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
-  const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
@@ -18,7 +12,7 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
+    return res.end();
   }
-  next();
+  return next();
 };
--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,12 @@ const UNAUTORIZED = 401;
 const SERVER_ERROR = 500;
 const JWT_SECRET = 'd90e6195ea595edeb43468b33997b7aeb3fd41c0e65ade2dbf8ccf2d299a5b77';
 const REGEX_LINK = /^https?:\/\/(www.)?\S+\.\S+#?/i;
+const allowedCors = [
+  'https://eve982.pet-project.nomoredomains.work',
+  'http://eve982.pet-project.nomoredomains.work',
+  'http://localhost:3000',
+];
 
 module.exports = {
-  BAD_REQUEST, UNAUTORIZED, SERVER_ERROR, CREATED, JWT_SECRET, REGEX_LINK,
+  BAD_REQUEST, UNAUTORIZED, SERVER_ERROR, CREATED, JWT_SECRET, REGEX_LINK, allowedCors
 };
